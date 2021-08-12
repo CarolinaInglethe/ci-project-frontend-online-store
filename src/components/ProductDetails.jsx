fa641095ebdbc35ee6fdc04422d7680dc5e83e52
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-08-11 21:52:26 -0300

    conclui requisito 9

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,32 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import ShoppingCartIcon from './ShoppingCartIcon';
-import { FaShoppingCart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+// import { FaShoppingCart } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
+import ShoppingCartIcon from './ShoppingCartIcon';
 
 class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { location } = this.props;
+    const { state } = location;
+    const { listProducts } = state.listProducts;
+    const { addToCart } = state.addToCart;
+    console.log(listProducts);
+
+    this.state = {
+      addToCart: [...addToCart],
+      listProducts,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    // const { location } = this.props;
+    // const { state } = location;
+    // const { addToCart } = state.addToCart;
+    const { addToCart: stateAddToCart, listProducts } = this.state;
+    const productId = event.target.id;
+    const filterProduct = listProducts.filter((result) => result.id === productId);
+    const [objectProduct] = filterProduct; // sem filtro: [...addToCart, objectProduct]
+    this.setState({
+      addToCart: [...stateAddToCart, objectProduct], // para ter filtro aqui newArray
+    });
+  }
+
   render() {
     const { location } = this.props;
     const { state } = location;
     const { product } = state.product;
+    const { addToCart, listProducts } = this.state;
+
     console.log(product);
+    console.log(listProducts);
     return (
       <div data-testid="product-detail-name">
-        <Link
+        <ShoppingCartIcon addToCart={ addToCart } />
+        {/* <Link
           to={ {
             pathname: '/shopping-cart',
             state: {
               product: [product],
+              addToCart: addToCart,
             },
           } }
           className="link"
           data-testid="shopping-cart-button"
         >
           <FaShoppingCart size="2vw" />
-        </Link>
+        </Link> */}
         <p>{ product.title }</p>
         <img src={ product.thumbnail } alt="foto" width="100px" />
         <p>{ product.price }</p>
+        <button
+          id={ product.id }
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.handleClick }
+        >
+          Adicionar ao Carrinho
+        </button>
       </div>
     );
   }
