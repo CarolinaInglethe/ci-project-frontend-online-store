3c2e12753d9a87bab3fc70582e0caf81ab3e6978
Isaddora Bayma <isadora.bayma@gmail.com>
2021-08-12 18:25:20 -0300

    requisito 12

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ShoppingCart from './components/ShoppingCart';
 import SearchBar from './components/SearchBar';
 // import ShoppingCartIcon from './components/ShoppingCartIcon';
 import ProductDetails from './components/ProductDetails';
+import CheckoutForm from './components/CheckoutForm';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             path="/product-details/:id"
             component={ ProductDetails }
           />
+          <Route path="/checkout" component={ CheckoutForm } />
         </Switch>
       </BrowserRouter>
     </div>
