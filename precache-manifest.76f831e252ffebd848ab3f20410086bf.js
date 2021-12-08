986fb90a4acad31093395ab8df1c4591ded83660
CarolinaInglethe <CarolinaInglethe@users.noreply.github.com>
2021-12-08 19:11:08 +0000

    deploy: c33f6ca8c553ff6ea5dc75dee195043ea0fc3f3d

---
+++ precache-manifest.76f831e252ffebd848ab3f20410086bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7a34b1c31dc56a532569566854ae86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55fd3c5fa4390c9e65a7",
+    "url": "./static/css/main.92c33fc8.chunk.css"
+  },
+  {
+    "revision": "417cab2a5a1e8e54864e",
+    "url": "./static/js/2.bd9ee1d0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.bd9ee1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fd3c5fa4390c9e65a7",
+    "url": "./static/js/main.f53b91bd.chunk.js"
+  },
+  {
+    "revision": "3bad4da90a1d62b2ed0b",
+    "url": "./static/js/runtime-main.6cc6b37c.js"
+  }
+]);
