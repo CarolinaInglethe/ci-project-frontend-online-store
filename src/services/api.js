10f30ff70bc5bdf6872b237a90a08fce04c4532a
CarolinaInglethe <carolinainglethe@gmail.com>
2026-06-19 16:30:10 -0300

    fix: handle API error response and prevent app crash

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,32 @@ const url = 'https://api.mercadolibre.com/sites/MLB';
 
 export async function getCategories() {
   const data = await fetch(`${url}/categories`)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
+    .then((response) => { 
+      if (!response.ok) {
+        throw new Error("Erro ao buscar categorias");
+      }
+      return response.json(); 
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
 
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const data = await fetch(`${url}/search?category=${categoryId}&q=${query}`)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
+    .then((response) => { 
+      if(!response.ok) {
+        throw new Error("Erro ao buscar produtos");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
 
   return data;
 }
