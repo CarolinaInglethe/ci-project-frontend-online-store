0d1842d992c87e0afa36ac4799405b90c556d83c
CarolinaInglethe <carolinainglethe@gmail.com>
2026-06-19 17:27:17 -0300

    wip: improve products fetch  witch mock fallback and filters

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,20 @@
+const categories = require("../__mocks__/categories");
+const productsMock = require("../__mocks__/query");
 const url = 'https://api.mercadolibre.com/sites/MLB';
+
 
 export async function getCategories() {
   const data = await fetch(`${url}/categories`)
     .then((response) => { 
       if (!response.ok) {
-        throw new Error("Erro ao buscar categorias");
+        throw new Error("API falhou, usando mock");
+        return categories;
       }
       return response.json(); 
     })
     .catch((error) => {
-      console.log(error);
-      return [];
+      console.log("Erro de rede , usando mock", error);
+      return categories;
     });
 
   return data;
@@ -20,7 +24,9 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const data = await fetch(`${url}/search?category=${categoryId}&q=${query}`)
     .then((response) => { 
       if(!response.ok) {
-        throw new Error("Erro ao buscar produtos");
+        throw new Error("API falhou , usando mock");
+        return productsMock.results
+        .filter((item) => item.category_id.toLowerCase().includes(categoryId.toLowerCase()))
       }
       return response.json();
     })
